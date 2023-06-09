a5861e0231afd0feebcd69a6b50c65b897d60f0c
Nick Munene <nickmunene101@gmail.com>
2023-06-09 11:47:30 +0300

    Finished on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,76 @@
-import Link from 'next/link'
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+
+import Logo from "./Logo";
+import { GithubIcon, LinkedInIcon, TwitterIcon } from "./Icons";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+
+  return (
+    <Link
+      href={href}
+      viewBox="0 0 512 512"
+      className={`${className} relative group`}
+    >
+      {title}
+      <span
+        className={`h-[2px] inline-block  bg-dark absolute left-0 -bottom-0.5 group-hover:w-full translate-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 const NavBar = () => {
   return (
-   <header
-   className='w-full px-32 py-8 font-medium flex items-center justify-between'
-   >
-    <nav>
-        <Link href="/">Home</Link>
-        <Link href="/">Home</Link>
-        <Link href="/">Home</Link>
-    </nav>
-    <nav>
-        <Link href="/">Home</Link>
-        <Link href="/">Home</Link>
-        <Link href="/">Home</Link>
-        <Link href="/">Home</Link>
-    </nav>
-   </header>
-  )
-}
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between relative">
+      <nav>
+        <CustomLink href={"/"} title={"Home"} className="mr-4" />
+        <CustomLink href={"/about"} title={"About"} className="mx-4" />
+        <CustomLink href={"/projects"} title={"Projects"} className="mx-4" />
+        <CustomLink href={"/contact"} title={"Contact Me"} className="ml-4" />
+      </nav>
 
-export default NavBar
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3"
+          href="https://github.com/NickM101"
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+          href="https://twitter.com/nick__nesh"
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3"
+          href="https://www.linkedin.com/in/nickmunene101/"
+        >
+          <LinkedInIcon />
+        </motion.a>
+      </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
