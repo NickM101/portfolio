0125453d0b8815672ad914be21f62a56a1c16cdc
Nick Munene <nickmunene101@gmail.com>
2023-06-12 11:42:02 +0300

    Added animated

--- src/components/Lilcon.js
+++ src/components/Lilcon.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { motion, useScroll } from "framer-motion";
+import React from "react";
 
-const Lilcon = () => {
+const Lilcon = ({ reference }) => {
+  const { scrollYProgress } = useScroll({
+    target: reference,
+    offset: ["center end", "center center"],
+  });
+
   return (
-    <div>Lilcon</div>
-  )
-}
+    <figure className="absolute left-0 stroke-dark">
+      <svg width={"75"} height={"75"} viewBox="0 0 100 100">
+        <circle
+          cx="75"
+          cy="50"
+          r="20"
+          className="stroke-primary stroke-1 fill-none"
+        />
+        <motion.circle
+          cx="75"
+          cy="50"
+          r="20"
+          className="stroke-[5px] fill-light"
+          style={{
+            pathLength: scrollYProgress,
+          }}
+        />
+        <circle cx="75" cy="50" r="10" className="animate-pulse stroke-1 fill-primary" />
+      </svg>
+    </figure>
+  );
+};
 
-export default Lilcon
+export default Lilcon;
