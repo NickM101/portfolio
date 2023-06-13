18ce7e811f0320f9efb8acbe38b54876af088d5b
Nick Munene <nickmunene101@gmail.com>
2023-06-13 11:43:27 +0300

    featured project added

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,48 @@
 import AnimatedText from "@/components/AnimatedText";
+import { GithubIcon } from "@/components/Icons";
 import Layout from "@/components/Layout";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import ginjuice from "../../public/images/ginjuice.webp";
+
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className="w-full flex items-center p-12 justify-center rounded-3xl border border-solid border-dark bg-light shadow-2xl">
+      <Link
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+        href={link}
+        target="_blank"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+        <p className="my-2 font-medium text-dark">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
+            Visit Projects
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
 
 const projects = () => {
   return (
@@ -12,18 +53,42 @@ const projects = () => {
       </Head>
       <main className="w-full mb-16 flex flex-col items-center justify-center">
         <Layout className="pt-16">
-            <AnimatedText text="Imagination Trumps Knowledge!" />
-            <div className="grid grid-cols-12 gap-24">
-                <div className="col-span-12">
-                    Featured Project
-                </div>
-                <div className="col-span-6">
-                    Featured Project
-                </div>
-                <div className="col-span-6">
-                    Featured Project
-                </div>
+          <AnimatedText text="Imagination Trumps Knowledge!" className="mb-16"/>
+          <div className="grid grid-cols-12 gap-24">
+            <div className="col-span-12">
+              <FeaturedProject
+                link={
+                  "https://play.google.com/store/apps/details?id=com.nexusnova.ginjuice"
+                }
+                github={"https://github.com/NickM101/GinJuice"}
+                img={ginjuice}
+                summary={
+                  "Elevate your mixology game with Gin Juice. Discover a world of exquisite gin-based cocktails crafted to perfection. From classic gin and tonics to innovative creations, our extensive library of recipes offers something for every taste. Our user-friendly interface makes cocktail creation a breeze, allowing you to explore, mix, and master the art of mixology."
+                }
+                title={"GinJuice"}
+                type={"Mobile"}
+              />
             </div>
+            <div className="col-span-6">Project-1</div>
+            <div className="col-span-6">Project-2</div>
+            <div className="col-span-12">
+              <FeaturedProject
+                link={
+                  "https://play.google.com/store/apps/details?id=com.nexusnova.ginjuice"
+                }
+                github={"https://github.com/NickM101/GinJuice"}
+                img={ginjuice}
+                summary={
+                  "Elevate your mixology game with Gin Juice. Discover a world of exquisite gin-based cocktails crafted to perfection. From classic gin and tonics to innovative creations, our extensive library of recipes offers something for every taste. Our user-friendly interface makes cocktail creation a breeze, allowing you to explore, mix, and master the art of mixology."
+                }
+                title={"GinJuice"}
+                type={"Mobile"}
+              />
+            </div>
+
+            <div className="col-span-6">Project -5</div>
+            <div className="col-span-6">Project -6</div>
+          </div>
         </Layout>
       </main>
     </>
